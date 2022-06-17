87f5c7b063e80ac10cb9ffe5357c6a5370feb869
kiran <konda.saik@northeastern.edu>
2022-06-17 00:22:56 -0400

    fizzbuzz game

---
+++ practice/fizzbuzz/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Println("FizzBuzz")
+	for i := 1 ;i <= 20 ;i++{
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("fizz buzz")
+		}else if i%3 == 0{
+			fmt.Println("fizz")
+		}else if i%5 == 0{
+			fmt.Println("buzz")
+		}else {
+			fmt.Println(i)
+		}
+		
+	}
+}
